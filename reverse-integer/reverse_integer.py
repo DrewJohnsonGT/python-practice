2c7a5e23a04f3cb34d29440a7f137ab0fcd59a13
Johnson <stephen.johnson@delta.com>
2020-03-09 08:58:35 -0400

    reverse integer

--- reverse-integer/reverse_integer.py
+++ reverse-integer/reverse_integer.py
@@ -8,11 +8,38 @@
 # Casting to string and reversing using
 # string indexing
 def reverse_int(x):
+    result = 0
     reversed_num = str(x)[::-1]
-    if x < 0:
-        return int('-' + reversed_num[:-1])
-    else:
-        return int(reversed_num)
+    result = int('-' + reversed_num[:-1]) if x < 0 else int(reversed_num)
+    return 0 if abs(result) > 0x7FFFFFFF else result
+
+# With integer math
+# use modulus to repeatedly 'pop'
+# integer and push in reverse to new value
+# check for overflow after
+# def reverse_int(x):
+#     digits = []
+#     if x == 0:
+#         return 0
+#     remains = abs(x)
+#     sign = -1 if x < 0 else 1
+#     while(True):
+#         # remains is not zero
+#         digit = remains % 10
+#         remains = remains // 10
+#         digits.append(digit)
+#         if remains == 0:
+#             break
+#     ret = 0
+#     for i in digits:
+#         ret *= 10
+#         ret += i
+
+#     ret *= sign
+#     if abs(ret) > 0x7FFFFFFF:
+#         return 0
+#     else:
+#         return ret
 
 
 def test_1():
