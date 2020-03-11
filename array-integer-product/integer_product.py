a4008f059a2bb1cb1ba68381caa029292f3993af
drewjohnsongt@gmail.com <drewjohnsongt@gmail.com>
2020-03-11 19:41:28 -0400

    needle and array to integer product

--- array-integer-product/integer_product.py
+++ array-integer-product/integer_product.py
@@ -4,18 +4,49 @@
 
 # Follow up, what if you can't use division?
 
+from functools import reduce
 
+# Brute force approach
+# calculate all values in entirety
+# def integer_product(nums):
+#     result = []
+#     for i in range(len(nums)):
+#         value = 1
+#         for j in range(len(nums)):
+#             if j != i:
+#                 value *= nums[j]
+#         result.append(value)
+#     return result
+
+# Simple optimization to first
+# calculate total product
+# divide by each integer for index value
+# def integer_product(nums):
+#     product = reduce(lambda x, y: x * y, nums)
+#     result = []
+#     for num in nums:
+#         result.append(product / num)
+#     return result
+
+
+# without division
+# multiply by inverse
+# same as above
 def integer_product(nums):
-    pass
+    product = reduce(lambda x, y: x * y, nums)
+    result = []
+    for num in nums:
+        result.append(product * (1 / num))
+    return result
 
 
 def test_1():
-    assert(integer_product([120, 60, 40, 30, 24]) == [1, 2, 3, 4, 5])
+    assert (integer_product([1, 2, 3, 4, 5]) == [120, 60, 40, 30, 24])
 
 
 def test_2():
-    assert(integer_product([3, 2, 1]) == [2, 3, 6])
+    assert (integer_product([3, 2, 1]) == [2, 3, 6])
 
 
 def test_3():
-    assert(integer_product([1, 2]) == [2, 1])
+    assert (integer_product([1, 2]) == [2, 1])
--- needle-in-haystack/needle.py
+++ needle-in-haystack/needle.py
@@ -2,7 +2,6 @@
 # Return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
 # Empty string return 0
 
-
 # Built in find operator
 # def strStr(haystack, needle):
 #     return haystack.find(needle)
@@ -23,24 +22,37 @@
 #         i += 1
 #     return -1
 
-# maintain map of first character of
-# needle in haystack
-# two pointers and jump
+
+# building match string
+# two pointers
 def strStr(haystack, needle):
     # Edge cases
     if not needle:
         return 0
     if len(needle) > len(haystack):
         return -1
+    i = 0
+
+    while i < len(haystack) - len(needle) + 1:
+        if haystack[i] == needle[0]:
+            # use j to check for reset of needle
+            n, j = '', 0
+            while j < len(needle) and haystack[i + j] == needle[j]:
+                n += haystack[i + j]
+                j += 1
+            if n == needle:
+                return i
+        i += 1
+    return -1
 
 
 def test_1():
-    assert(strStr(haystack="hello", needle="ll") == 2)
+    assert (strStr(haystack="hello", needle="ll") == 2)
 
 
 def test_2():
-    assert(strStr(haystack="aaaaa", needle="bba") == -1)
+    assert (strStr(haystack="aaaaa", needle="bba") == -1)
 
 
 def test_3():
-    assert(strStr(haystack="testingthis", needle="this") == 7)
+    assert (strStr(haystack="testingthis", needle="this") == 7)
